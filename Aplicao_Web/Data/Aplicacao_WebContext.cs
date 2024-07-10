f718bf8d05d96beefc1a33bf8c40b29e593e1975
Caio Elias <caioelias.ptu@gmail.com>
2024-07-10 15:21:39 -0300

    Criação de crud Department via scaffold

---
+++ Aplicao_Web/Data/Aplicacao_WebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Aplicacao_Web.Models;
+
+namespace Aplicacao_Web.Data
+{
+    public class Aplicacao_WebContext : DbContext
+    {
+        public Aplicacao_WebContext (DbContextOptions<Aplicacao_WebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aplicacao_Web.Models.Department> Department { get; set; } = default!;
+    }
+}
--- Aplicao_Web/Program.cs
+++ Aplicao_Web/Program.cs
@@ -1,33 +1,41 @@
-internal class Program
+using Microsoft.EntityFrameworkCore;
+using Aplicacao_Web.Data;
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Aplicacao_WebContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Aplicacao_WebContext") ?? throw new InvalidOperationException("Connection string 'Aplicacao_WebContext' not found.")));
+
+
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    private static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-        builder.Services.AddControllersWithViews();
-
-        var app = builder.Build();
-
-        // Configure the HTTP request pipeline.
-        if (!app.Environment.IsDevelopment())
-        {
-            app.UseExceptionHandler("/Home/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-
-        app.Run();
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//Seed database
+
+
+app.Run();
